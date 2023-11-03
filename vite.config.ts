03e8396b8d360d6e6defdca5c3ac8b62dbec9591
Nicole Nobles <shecodez@gmail.com>
2023-11-02 20:25:50 -0700

    📦 chore: add pkgs to improve dx

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
